24988ce6dfdd1832057a2f7bae543698397e78d6
Stephany Mendes <56844567+SteScarpel@users.noreply.github.com>
2025-03-06 11:54:10 -0300

    Primeiro commit do projeto

---
+++ app/src/main/java/com/example/myapplicationbotao/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.myapplicationbotao;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import android.widget.Button;
+import android.widget.Toast;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Button btnToast = findViewById(R.id.button);
+        btnToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Botão clicado!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
